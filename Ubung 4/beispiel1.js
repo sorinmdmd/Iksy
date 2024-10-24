0326de824917987b690405e23383a97a57e2b711
Sorin Otel <otelsorin@gmail.com>
2024-10-24 18:40:48 +0200

    Third

---
+++ Ubung 4/beispiel1.js
@@ -0,0 +1,11 @@
+var bisZu;
+var summe;
+bisZu = prompt("Bitte geben Sie Zahl, bis zu der summiert wird, ein", "");
+bisZu = parseInt(bisZu);
+summe = 0;
+for (i = 1; i <= bisZu; i++) {
+  summe = summe + i;
+}
+document.write(
+  "Die Summe der ersten " + bisZu + " natürlichen Zahlen " + " ist: " + summe
+);
